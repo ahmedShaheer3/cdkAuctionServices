39b0024de08a99a75eb76f64cd5b1276b4d38f0d
ahmedShaheer3 <122429039+ahmedShaheer3@users.noreply.github.com>
2024-07-02 13:40:37 +0500

    Initial commit

---
+++ test/cdk_auction.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkAuction from '../lib/cdk_auction-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk_auction-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkAuction.CdkAuctionStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
